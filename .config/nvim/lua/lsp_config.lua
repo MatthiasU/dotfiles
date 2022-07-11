31cd767af44ba9686208a751655a34c8798de3ec
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2022-07-11 22:32:57 +0200

    Add detail completion for clangd

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -88,6 +88,7 @@ end
  -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   require('lspconfig')['clangd'].setup {
+    cmd = { "clangd", "--completion-style=detailed" },
     capabilities = capabilities,
     on_attach = on_attach,
   }
