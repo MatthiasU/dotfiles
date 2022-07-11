ad97ddabfa4dc7c831db9cbc10e9dc3fe250d538
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2022-07-11 21:18:59 +0200

    Add on attach to lsp

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -88,11 +88,14 @@ end
  -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   require('lspconfig')['clangd'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
   }
   require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
   }
   require('lspconfig')['cmake'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
   }
