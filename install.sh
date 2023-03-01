efa5e11801fae6e34fb7aa378654d05a9376700d
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2023-03-01 23:10:30 +0100

    Add packer install to install script

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ if [ -d "$NVIM_DIR" ]; then
     rm -rf ~/.config/nvim
 fi
 
+PACKER_REPO="https://github.com/wbthomason/packer.nvim"
+PACKER_DIR="$HOME/.local/share/nvim/site/pack/packer/start"
+
+# install packer pacakge manager
+if [ ! -d "$PACKER_DIR" ]; then
+    git clone --depth 1 $PACKER_REPO $PACKER_DIR/packer.nvim
+fi
+
 if [ -d "$TMUX_DIR" ]; then
     rm -rf ~/.config/tmux
 fi
