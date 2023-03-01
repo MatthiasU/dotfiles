873e96ce33d83a2300f359e0d55dda788e13acc8
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2023-03-01 22:58:31 +0100

    refactor telescope

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -3,11 +3,12 @@ local builtin = require('telescope.builtin')
 local Remap = require("matthiasu.keymap")
 local nnoremap = Remap.nnoremap
 
-nnoremap('<leader>pf', builtin.find_files)
-nnoremap('<C-p>', builtin.git_files)
+nnoremap('<leader>ff', builtin.find_files)
+nnoremap('<leader>p', builtin.git_files)
 
 require('telescope').setup{
 	defaults = {
-		path_display={"smart"}
+		path_display={"smart"},
+        file_ignore_patterns = { "build", ".cache" }
 	}
 }
