56a3faadc83ec70f089a904f3a263a0eac5f8768
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2025-08-19 23:13:16 +0200

    new nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,10 +15,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+
 require('lazy').setup({
    spec = {
 	    -- Colorscheme
 	    { "catppuccin/nvim", name = "catppuccin" },
+	    { "rose-pine/neovim", name = "rose-pine" },
 
 	    -- LSP configuration
 	    { "neovim/nvim-lspconfig" },
@@ -35,6 +37,15 @@ require('lazy').setup({
 	    { "hrsh7th/cmp-nvim-lsp" },
 	    { "hrsh7th/cmp-vsnip" },
 	    { "hrsh7th/vim-vsnip" },
+
+        { "preservim/nerdtree" },
+
+        -- Comment
+        { "numToStr/Comment.nvim"}, 
+
+        -- Git 
+        { "tpope/vim-fugitive"}, 
+
     },
     checker = { enabled = true },
 })
@@ -42,7 +53,7 @@ require('lazy').setup({
 
 vim.o.syntax = 'on'
 vim.o.compatible = false
-
+vim.o.signcolumn='yes'
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 4
@@ -57,22 +68,42 @@ vim.o.background = 'dark'
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.wildmenu = true
-vim.o.hlsearch = false
-
+vim.o.hlsearch = true
 vim.g.mapleader = ' '
+vim.g.netrw_liststyle = 3
 
 vim.keymap.set('n', '<leader>w', ':update<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>o', ':source<CR>', { noremap = true, silent = true })
 
 -- UI 
---
-vim.cmd.colorscheme('catppuccin')
+
+require('catppuccin').setup({
+    flavour = 'macchiato',
+    background = { dark },
+    transparent_background = true,
+})
+
+require('rose-pine').setup({
+    variant = "moon", -- auto, main, moon, or dawn
+    extend_background_behind_borders = true,
+    dark_variant = "main", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    styles = {
+        bold = true,
+        italic = false,
+        transparency = false,
+    },
+})
+
+
+vim.cmd.colorscheme('rose-pine')
 vim.o.winborder = 'rounded'
 
 -- Fuzzy finder
 
 vim.keymap.set('n', '<C-p>', ':GFiles<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ff', ':Files<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>b', ':Buffers<CR>', { noremap = true, silent = true })
 
 -- LSP
 
@@ -80,11 +111,20 @@ local lspconfig = require('lspconfig')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
+-- Terminal
+vim.keymap.set('t', '<Esc>', ':<C-\\><C-n>', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<leader>t', ':tabnew term://zsh<CR>', { noremap = true, silent = true })
+
 lspconfig.clangd.setup({
   capabilities = capabilities,
 })
 
 lspconfig.pyright.setup({
+  capabilities = capabilities,
+})
+
+
+lspconfig.cmake.setup({
   capabilities = capabilities,
 })
 
@@ -103,6 +143,13 @@ cmp.setup({
 })
 
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', 'gca', vim.lsp.buf.code_action, { noremap = true, silent = true })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true })
+vim.keymap.set('n', 'gn', vim.lsp.buf.rename, { noremap = true, silent = true })
+
+
 
 -- Linter
 
@@ -111,8 +158,10 @@ lint.linters_by_ft = {
     python = { 'ruff' }
 }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
+-- NERD Tree
+
+vim.cmd([[ let g:NERDTreeWinPos = "right" ]])
+vim.keymap.set('n', '<leader>n', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>t', ':NERDTreeFind<CR>', { noremap = true, silent = true })
+
+
