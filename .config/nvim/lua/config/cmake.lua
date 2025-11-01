9e073fafc982bb9c3d694c2ecddb96c56ca2b08c
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2025-11-01 18:08:50 +0100

    Refine cmake macros

--- .config/nvim/lua/config/cmake.lua
+++ .config/nvim/lua/config/cmake.lua
@@ -1,22 +1,16 @@
 
 vim.api.nvim_create_user_command('CMakeBuild',
     function()
-        vim.cmd.vnew()
         vim.cmd.term("cmake --build ./build --config Debug")
-        vim.cmd.wincmd("J")
-        vim.api.nvim_win_set_height(0, 5)
     end,
     {}
 )
 
 vim.api.nvim_create_user_command('CMakeTest',
     function()
-        vim.cmd.vnew()
         vim.cmd.term("cmake --build ./build --config Debug && ctest --test-dir ./build --output-on-failure")
-        vim.cmd.wincmd("J")
-        vim.api.nvim_win_set_height(0, 20)
     end,
     {}
 )
 
-
+vim.keymap.set('n', '<leader>r', ':CMakeTest<CR>', { noremap = true, silent = true })
