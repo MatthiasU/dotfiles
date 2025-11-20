5227df19f0575c6a8c1aa11a8e8679339bff01ba
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2025-11-20 20:18:08 +0100

    Add python debugger

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -12,6 +12,12 @@ dap.adapters.lldb = {
     name = "lldb"
 }
 
+dap.adapters.debugpy = {
+    type = 'executable',
+    command = '.venv/bin/python',
+    args = { '-m', 'debugpy.adapter' },
+}
+
 vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.toggle() end, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end, { noremap = true, silent = true })
@@ -21,15 +27,36 @@ vim.keymap.set('n', '<Leader>do', function() require('dap').step_out() end, { no
 vim.keymap.set('n', '<Leader>dt', function() require('dap').terminate() end, { noremap = true, silent = true })
 
 dap.configurations.cpp = {
+    name = "Launch Executable",
+    type = "lldb",
+    request = "launch",
+    program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+}
+
+dap.configurations.python = {
     {
+        type = 'debugpy',
+        request = 'launch',
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+            return '.venv/bin/python'
+        end,
+    },
+    {
+        type = 'debugpy',
+        request = 'launch',
         name = "Launch Executable",
-        type = "lldb",
-        request = "launch",
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = {},
+        pythonPath = function()
+            return '.venv/bin/python'
+        end,
     },
 }
