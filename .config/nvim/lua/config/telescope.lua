b7eccdeef7ac9e5d784ffe9b4743417cf0f1f481
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2025-11-25 21:16:15 +0100

    Enable hidden git files to be searchable for fuzzy finder

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,5 +1,16 @@
+local telescope = require('telescope')
+
+telescope.load_extension('fzf')
+telescope.setup {
+    pickers = {
+        git_files = {
+            hidden = true
+        }
+    }
+}
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'find files' })
+vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'help tags' })
