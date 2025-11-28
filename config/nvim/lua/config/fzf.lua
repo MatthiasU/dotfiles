c8dfbdf80c0a5d8b26ba940d0633870b45673db4
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2025-11-28 19:32:08 +0100

    Add better quickfix list handling with fzf

--- config/nvim/lua/config/fzf.lua
+++ config/nvim/lua/config/fzf.lua
@@ -2,9 +2,19 @@ vim.keymap.set('n', '<leader>ff', ':Files<CR>', { desc = 'find files' })
 vim.keymap.set('n', '<leader>fg', ':Rg<CR>', { desc = 'live grep' })
 vim.keymap.set('n', '<leader>fb', ':Buffers<CR>', { desc = 'buffers' })
 vim.keymap.set('n', '<leader>fh', ':HelpTags<CR>', { desc = 'help tags' })
+local function build_quickfix_list(lines)
+    vim.fn.setqflist(
+        vim.tbl_map(function(line)
+            return { filename = line, lnum = 1 }
+        end, vim.list_slice(lines, 1, #lines)),
+        'r'
+    )
+    vim.cmd.copen()
+    vim.cmd.cc()
+end
 
 vim.g.fzf_action = {
-    ['ctrl-q'] = vim.fn['s:build_quickfix_list'],
+    ['ctrl-q'] = build_quickfix_list,
     ['ctrl-t'] = 'tab split',
     ['ctrl-x'] = 'split',
     ['ctrl-v'] = 'vsplit',
