1688199e668add585b03f8eb387d35b95b8e320c
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-02-26 22:19:49 +0100

    Add fast copy keybinds (commented out)

--- config/nvim/lua/config/clipboard.lua
+++ config/nvim/lua/config/clipboard.lua
@@ -1,1 +1,6 @@
 vim.o.clipboard = "unnamedplus"
+
+-- Copy to system clipboard with Leader + y
+-- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- Paste from system clipboard with Leader + p
+-- vim.keymap.set("n", "<leader>p", [["+p]])
