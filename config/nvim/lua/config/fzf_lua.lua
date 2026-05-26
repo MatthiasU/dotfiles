1d093ea6703a625cc6b0ad4c308a584676dc808f
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-05-26 21:20:53 +0200

    Configure fzf_lua

--- config/nvim/lua/config/fzf_lua.lua
+++ config/nvim/lua/config/fzf_lua.lua
@@ -1,27 +1,14 @@
-require("fzf-lua").setup()
-
-local function build_quickfix_list(lines)
-    vim.fn.setqflist(
-        vim.tbl_map(function(line)
-            return { filename = line, lnum = 1 }
-        end, vim.list_slice(lines, 1, #lines)),
-        'r'
-    )
-    vim.cmd.copen()
-    vim.cmd.cc()
-end
-
-vim.g.fzf_action = {
-    ['ctrl-q'] = build_quickfix_list,
-    ['ctrl-t'] = 'tab split',
-    ['ctrl-x'] = 'split',
-    ['ctrl-v'] = 'vsplit',
-}
+local fzf_lua = require("fzf-lua")
+fzf_lua.setup()
 
 vim.g.fzf_layout = { window = { width = 0.8, height = 0.5, xoffset = 0.5 } }
 
-vim.keymap.set('n', '<leader>ff', ':FzfLua global<CR>', { desc = 'find git files' })
-vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>', { desc = 'buffers' })
-vim.keymap.set('n', '<leader>fl', ':FzfLua blines<CR>', { desc = 'search in file' })
+vim.keymap.set('n', '<leader>ff', function() fzf_lua.global() end, { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>fb', function() fzf_lua.buffers() end, { desc = 'Find Buffers' })
+vim.keymap.set('n', '<leader>fl', function() fzf_lua.blines() end, { desc = 'Search in file' })
 
-vim.keymap.set('n', '<leader>sh', ':FzfLua helptags<CR>', { desc = 'search in file' })
+vim.keymap.set('n', '<leader>sh', function() fzf_lua.help_tags() end, { desc = 'Search Help Tags' })
+
+vim.keymap.set('n', '<leader>fq', function() fzf_lua.quickfix() end, { desc = 'Search in QuickFixlist' })
+
+vim.keymap.set('n', '<leader>gb', function() fzf_lua.git_branches() end, { desc = 'Git branches' })
