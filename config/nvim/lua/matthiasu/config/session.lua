1209be76c45489bc4bc1d53cb807ac3d3c6964ae
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-06-19 17:44:54 +0200

    Change session commands to create only local sessions

--- config/nvim/lua/matthiasu/config/session.lua
+++ config/nvim/lua/matthiasu/config/session.lua
@@ -1,13 +1,17 @@
+local default_session_file = ".session.vim"
+local save_session_cmd = ":mksession! " .. default_session_file
+local load_session_cmd = ":source " .. default_session_file
+
 vim.api.nvim_create_user_command('SessionSave',
     function()
-        vim.cmd(":mksession! $HOME/.config/nvim/session.vim")
+        vim.cmd(save_session_cmd)
     end,
     {}
 )
 
 vim.api.nvim_create_user_command('SessionLoad',
     function()
-        vim.cmd(":source $HOME/.config/nvim/session.vim")
+        vim.cmd(load_session_cmd)
     end,
     {}
 )
