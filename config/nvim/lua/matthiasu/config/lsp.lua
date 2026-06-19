b87f6d60c9b54541699adbd86703d2b34945f923
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-06-19 18:21:25 +0200

    Improve lsp settings (unify attach command)

--- config/nvim/lua/matthiasu/config/lsp.lua
+++ config/nvim/lua/matthiasu/config/lsp.lua
@@ -1,24 +1,46 @@
 require("mason").setup()
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('lsp', {}),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end,
-})
+local lsp_group = vim.api.nvim_create_augroup('lsp_attach_group', { clear = true })
+local lsp_fmt_group = vim.api.nvim_create_augroup('lsp_format_group', { clear = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
+    group = lsp_group,
     callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        local bufnr = args.buf
+
+        -- 1. Enable Native Completion
+        vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+
+        -- 2. Format on Save
         if client:supports_method('textDocument/formatting') then
+            vim.api.nvim_clear_autocmds({ group = lsp_fmt_group, buffer = bufnr })
             vim.api.nvim_create_autocmd('BufWritePre', {
-                buffer = args.buf,
+                group = lsp_fmt_group,
+                buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, async = false })
+                    vim.lsp.buf.format({ bufnr = bufnr, async = false })
                 end,
             })
         end
+
+        -- 3. Buffer-Local Keymaps
+        local opts = { buffer = bufnr, silent = true }
+
+        -- Utilizing fzf-lua for richer navigation
+        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true })
+        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+        -- vim.keymap.set('n', 'gca', vim.lsp.buf.code_action, { noremap = true, silent = true })
+
+        vim.keymap.set('n', 'gd', ":FzfLua lsp_definitions<cr>", opts)
+        vim.keymap.set('n', 'gr', ":FzfLua lsp_references<cr>", opts)
+        vim.keymap.set('n', 'gca', ":FzfLua lsp_code_actions<cr>", opts)
+        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', 'gn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gq', vim.lsp.formatexpr, opts)
+        vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, opts)
     end,
 })
 
@@ -55,16 +77,6 @@ vim.lsp.config('lua_ls', {
     },
 })
 
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { noremap = true, silent = true })
-vim.keymap.set('n', 'gca', vim.lsp.buf.code_action, { noremap = true, silent = true })
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true })
-vim.keymap.set('n', 'gn', vim.lsp.buf.rename, { noremap = true, silent = true })
-vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { noremap = true, silent = true })
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
-vim.keymap.set('n', 'gq', vim.lsp.formatexpr, { noremap = true, silent = true })
-vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { noremap = true, silent = true })
-
 vim.keymap.set('i', '<C-space>', function()
     vim.lsp.completion.get()
 end)
@@ -83,3 +95,4 @@ vim.lsp.enable('superhtml')
 vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('clangd')
 vim.lsp.enable('cmake')
+
