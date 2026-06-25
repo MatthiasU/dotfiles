7c582b583c7a3b57f00602ba2c6ffd0c8ba77f5b
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-06-25 20:23:05 +0200

    add: keybindings for gitsigns

--- config/nvim/lua/matthiasu/config/gitsigns.lua
+++ config/nvim/lua/matthiasu/config/gitsigns.lua
@@ -1,1 +1,17 @@
-require('gitsigns').setup({})
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+        local opts = { noremap = true, silent = true }
+        map('n', '<leader>hs', gitsigns.stage_hunk, opts)
+        map('n', '<leader>hS', gitsigns.stage_buffer, opts)
+        map('n', '<leader>hr', gitsigns.reset_hunk, opts)
+        map('n', '<leader>hp', gitsigns.preview_hunk, opts)
+        map('n', '<leader>hd', gitsigns.diffthis, opts)
+    end
+})
