7ca53b972f23da6dcbc5e7906f43b728867b6c3b
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-06-25 19:27:59 +0200

    add: keybindings for fast substitute command

--- config/nvim/lua/matthiasu/config/keybindings.lua
+++ config/nvim/lua/matthiasu/config/keybindings.lua
@@ -11,3 +11,4 @@ vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
