8e7c358d732f88d1fe410148cd5ff74837b0b136
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-06-25 19:27:10 +0200

    fix: add opts local for keybindings

--- config/nvim/lua/matthiasu/config/keybindings.lua
+++ config/nvim/lua/matthiasu/config/keybindings.lua
@@ -1,7 +1,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set('n', '<leader>w', ':update<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>o', ':source<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '<leader>w', ':update<CR>', opts)
+vim.keymap.set('n', '<leader>o', ':source<CR>', opts)
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', opts)
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
+
