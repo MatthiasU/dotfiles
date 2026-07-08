36c3bf5a7b54bd1197ff435ef3898131f6738e96
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-07-08 13:27:16 +0200

    add keybind for conditional breakpoint

--- config/nvim/lua/matthiasu/config/dap.lua
+++ config/nvim/lua/matthiasu/config/dap.lua
@@ -17,6 +17,11 @@ dap.adapters.debugpy = {
 }
 
 vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end, { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>dB',
+    function()
+        local condition = vim.fn.input("Breakpoint condition: ")
+        require('dap').toggle_breakpoint(condition)
+    end, { noremap = true, silent = true, desc = "Toggle conditional breakpoint" })
 vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.toggle() end, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>dn', function() require('dap').step_over() end, { noremap = true, silent = true })
