b81058ccc9e2da04541aaa71708850d36c994a45
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-07-10 18:54:02 +0200

    add visual mode keybinds for gitsigns

--- config/nvim/lua/matthiasu/config/gitsigns.lua
+++ config/nvim/lua/matthiasu/config/gitsigns.lua
@@ -13,5 +13,13 @@ require('gitsigns').setup({
         map('n', '<leader>hr', gitsigns.reset_hunk, opts)
         map('n', '<leader>hp', gitsigns.preview_hunk, opts)
         map('n', '<leader>hd', gitsigns.diffthis, opts)
+
+        map('v', '<leader>hs', function()
+            gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
+
+        map('v', '<leader>hr', function()
+            gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
     end
 })
