85e1e96c04e61e20f690463430e33e11356327f4
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-07-31 16:29:39 +0200

    add bazel lsp

--- config/nvim/lua/matthiasu/config/lsp.lua
+++ config/nvim/lua/matthiasu/config/lsp.lua
@@ -76,6 +76,7 @@ vim.lsp.enable('clangd')
 vim.lsp.enable('cmake')
 vim.lsp.enable('ruff')
 vim.lsp.enable('pyrefly')
+vim.lsp.enable('starpls')
 
 vim.diagnostic.config({
     severity_sort = true,
