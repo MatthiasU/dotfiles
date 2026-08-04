225197fac4f04657bfe944944ec9230980e66323
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-08-04 23:20:59 +0200

    remove deprecated live grep resume keymap

--- config/nvim/lua/matthiasu/config/fzf_lua.lua
+++ config/nvim/lua/matthiasu/config/fzf_lua.lua
@@ -21,7 +21,6 @@ vim.keymap.set('n', '<leader>fm', function() fzf_lua.marks() end, { desc = 'Find
 vim.keymap.set('n', '<leader>sl', function() fzf_lua.history() end, { desc = 'Search latest files' })
 vim.keymap.set('n', '<leader>sh', function() fzf_lua.help_tags() end, { desc = 'Search Help Tags' })
 vim.keymap.set('n', '<leader>fs', function() fzf_lua.live_grep() end, { desc = 'Search with live grep' })
-vim.keymap.set('n', '<leader>sr', function() fzf_lua.live_grep_resume() end, { desc = 'Resume live grep' })
 
 vim.keymap.set('n', '<leader>fq', function() fzf_lua.quickfix() end, { desc = 'Search in QuickFixlist' })
 vim.keymap.set('n', '<leader>gb', function() fzf_lua.git_branches() end, { desc = 'Git branches' })
