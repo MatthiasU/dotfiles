1cff626157962bd4bb34955973a51d6bc1a37a84
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-08-26 10:00:23 +0200

    Add current cmd into lualine

--- config/nvim/lua/matthiasu/config/lualine.lua
+++ config/nvim/lua/matthiasu/config/lualine.lua
@@ -1,1 +1,7 @@
-require('lualine').setup()
+vim.opt.showcmd = true
+vim.opt.showcmdloc = "statusline"
+require('lualine').setup({
+  sections = {
+    lualine_x = { '%S', 'encoding', 'fileformat', 'filetype' }
+  }
+})
