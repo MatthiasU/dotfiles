cd087a5ae13299cd5cfffd8212c11f315f60420c
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-08-28 09:09:40 +0200

    update fzf keybinds

--- config/nvim/lua/matthiasu/config/fzf_lua.lua
+++ config/nvim/lua/matthiasu/config/fzf_lua.lua
@@ -1,5 +1,11 @@
 local fzf_lua = require("fzf-lua")
 fzf_lua.setup({
+    winopts = {
+					split = "belowright 18new",
+					preview = {
+						hidden = true,
+					}
+				},
     keymap = {
         fzf = {
             ["ctrl-q"] = "select-all+accept",
@@ -8,18 +14,19 @@ fzf_lua.setup({
     fzf_opts = {
         ["--tiebreak"] = "begin,length",
         ["--history"] = vim.fn.stdpath("data") .. "/fzf_history",
+        ["--layout"] = "default",
     },
     ui_select = true
 })
 
-vim.keymap.set('n', '<leader>ff', function() fzf_lua.files() end, { desc = 'Find Files' })
-vim.keymap.set('n', '<leader>fb', function() fzf_lua.buffers() end, { desc = 'Find Buffers' })
+vim.keymap.set('n', '<C-p>', function() fzf_lua.files() end, { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>;', function() fzf_lua.buffers() end, { desc = 'Find Buffers' })
 vim.keymap.set('n', '<leader>fl', function() fzf_lua.blines() end, { desc = 'Search in file' })
 vim.keymap.set('n', '<leader>fm', function() fzf_lua.marks() end, { desc = 'Find marks' })
 
 vim.keymap.set('n', '<leader>sl', function() fzf_lua.history() end, { desc = 'Search latest files' })
 vim.keymap.set('n', '<leader>sh', function() fzf_lua.help_tags() end, { desc = 'Search Help Tags' })
-vim.keymap.set('n', '<leader>fs', function() fzf_lua.live_grep() end, { desc = 'Search with live grep' })
+vim.keymap.set('n', '<leader>rg', function() fzf_lua.live_grep() end, { desc = 'Search with live grep' })
 
 vim.keymap.set('n', '<leader>fq', function() fzf_lua.quickfix() end, { desc = 'Search in QuickFixlist' })
 vim.keymap.set('n', '<leader>gb', function() fzf_lua.git_branches() end, { desc = 'Git branches' })
