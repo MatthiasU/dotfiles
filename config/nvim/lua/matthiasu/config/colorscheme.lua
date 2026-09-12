d8614e8795280559527b3184d5ee679cdb519cfb
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-09-12 12:07:40 +0200

    add auto switching for background with kanagwawa + dimming

--- config/nvim/lua/matthiasu/config/colorscheme.lua
+++ config/nvim/lua/matthiasu/config/colorscheme.lua
@@ -6,7 +6,7 @@ require('kanagawa').setup({
     statementStyle = { bold = true },
     typeStyle = {},
     transparent = false,   -- do not set background color
-    dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+    dimInactive = true,    -- dim inactive window `:h hl-NormalNC`
     terminalColors = true, -- define vim.g.terminal_color_{0,17}
     theme = "wave",        -- Load "wave" theme
     background = {         -- map the value of 'background' option to a theme
@@ -22,6 +22,18 @@ require('kanagawa').setup({
             }
         }
     }
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function(ev)
+        if ev.match == "kanagawa-lotus" then
+            vim.o.background = 'light'
+        end
+        if ev.match == "kanagawa" or ev.match == "kanagawa-dragon" or ev.match == "kanagawa-wave" then
+            vim.o.background = 'dark'
+        end
+    end,
 })
 
 vim.cmd.colorscheme('kanagawa')
