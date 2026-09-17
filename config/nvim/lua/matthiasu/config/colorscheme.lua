7e37bf709ac6089bda10c10aa445629d025dc389
Matthias Uttendorfer <uttendorfermatthias@icloud.com>
2026-09-17 18:23:22 +0200

    Default to light mode

--- config/nvim/lua/matthiasu/config/colorscheme.lua
+++ config/nvim/lua/matthiasu/config/colorscheme.lua
@@ -8,7 +8,7 @@ require('kanagawa').setup({
     transparent = false,   -- do not set background color
     dimInactive = true,    -- dim inactive window `:h hl-NormalNC`
     terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    theme = "wave",        -- Load "wave" theme
+    theme = "lotus",       -- Load "wave" theme
     background = {         -- map the value of 'background' option to a theme
         dark = "wave",     -- try "dragon" !
         light = "lotus"
@@ -36,4 +36,4 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     end,
 })
 
-vim.cmd.colorscheme('kanagawa')
+vim.cmd.colorscheme('kanagawa-lotus')
